937f6b0628027b4f151d4dab31e109951ea4ac10
Azvedo-06 <azinho006@gmail.com>
2024-08-28 13:53:14 -0300

    tirando ex da main

--- Trilha02/Universidade.java
+++
@@ -1,9 +0,0 @@
-package Main.Trilha02;
-
-public class Universidade {
-    String nomeUNI;
-    String local;
-    public void mostrarNome() {
-        System.out.println("Nome da Universidade: " + nomeUNI);
-    }
-}
